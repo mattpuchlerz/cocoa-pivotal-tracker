3e1dfb926a79e406e9311ed532d8431edef71ad6
Matt Puchlerz <matt@puchlerz.com>
2009-10-07 08:08:05 -0400

    Renamed and redescribed

--- cocoa-pivotal-tracker.user.js
+++ cocoa-pivotal-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           Cocoa Tracker
-// @namespace      http://github.com/mattpuchlerz/cocoa-tracker
-// @description    Redesigns Pivotal Tracker to appear more like a Cocoa application.
+// @name           Cocoa Pivotal Tracker
+// @namespace      http://github.com/mattpuchlerz/cocoa-pivotal-tracker
+// @description    Re-skins Pivotal Tracker to appear more like a Cocoa application.
 // @include        http://www.pivotaltracker.com/*
 // @include        https://www.pivotaltracker.com/*
 // ==/UserScript==
@@ -9,7 +9,7 @@
 var link   = document.createElement('link');
 link.type  = 'text/css';
 link.rel   = 'stylesheet';
-link.href  = 'http://matt.puchlerz.com/cocoa-tracker/cocoa-tracker.css';
+link.href  = 'http://mattpuchlerz.github.com/cocoa-pivotal-tracker/cocoa-pivotal-tracker.css';
 link.media = 'screen';
 link.title = 'Cocoa Tracker';
 link.id    = 'cocoa-tracker-link';
